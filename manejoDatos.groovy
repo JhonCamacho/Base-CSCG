97cc16d5c58c469d2bb8d8c196c514f54ff1b2a2
Jhon Camacho <jhonfcp@hotmail.com>
2023-12-01 17:29:16 -0500

    Integracion de funciones de validacion y contratacion.

--- manejoDatos.groovy
+++ manejoDatos.groovy
@@ -1,24 +1,24 @@
 //base de datos
 def baseEmpresa = [:]
 //Ingreso de empleados a base
-baseEmpresa['1014230323']=['Nombre':'Luis','apellido':'Calderon','segnom':'Carlos','nickname':'Lucacal',
-'Empleos':[["Cargo":"Gerente de proyecto","Años":2,"Empresa":"Formarte"],["Cargo":"Supervisor","años":3,"Empresa":"ArteBro"],["Cargo":"Operario","años":5,"Empresa":"ArteCroods"]]
+baseEmpresa['1014230323']=['Nombre':'Luis','Apellido':'Calderon','Segnom':'Carlos','Nickname':'Lucacal',
+'Empleos':[["Cargo":"Gerente de proyecto","Años":2,"Empresa":"Formarte"],["Cargo":"Supervisor","Años":3,"Empresa":"ArteBro"],["Cargo":"Operario","Años":5,"Empresa":"ArteCroods"]]
  ,"Direccion":"Calle 80 Diagonal 30 - 12"]
  
 baseEmpresa['72345667']=['Nombre':'Rodolfo','Apellido':'Perez','Segnom':'Arturo','Nickname':'Ropea',
-'Empleos':[["Cargo":"Agente de obra","Años":2,"Empresa":"ConstruNow"],["Cargo":"Maestro","años":5,"Empresa":"RalphDem"],["Cargo":"Ayudante","años":5,"Empresa":"IntConstru"]]
+'Empleos':[["Cargo":"Agente de obra","Años":2,"Empresa":"ConstruNow"],["Cargo":"Maestro","Años":5,"Empresa":"RalphDem"],["Cargo":"Ayudante","Años":5,"Empresa":"IntConstru"]]
  ,"Direccion":"Calle LAJ 26 # 30 -15"]
 
-baseEmpresa['1012345211']=['Nombre':'Fabio','apellido':'Cruz','segnom':'Alberto','nickname':'Falcru',
-'Empleos':[["Cargo":"Supervisor SST","Años":1,"Empresa":"Segurity"],["Cargo":"Analista","años":5,"Empresa":"AnalisetaInc"],["Cargo":"Aprendiz","años":2,"Empresa":"Aprend+"]]
+baseEmpresa['1012345211']=['Nombre':'Fabio','Apellido':'Cruz','Segnom':'Alberto','Nickname':'Falcru',
+'Empleos':[["Cargo":"Supervisor SST","Años":1,"Empresa":"Segurity"],["Cargo":"Analista","Años":5,"Empresa":"AnalisetaInc"],["Cargo":"Aprendiz","Años":2,"Empresa":"Aprend+"]]
  ,"Direccion":"Carrera 35 # 23-80 Edf.Balcones t2 apt311"]
  
-baseEmpresa['101567786']=['Nombre':'Paola','apellido':'Ruiz','segnom':'Andrea','nickname':'Panru',
-'Empleos':[["Cargo":"Secretaria","Años":2,"Empresa":"IndustriaInc"],["Cargo":"Auxiliar","años":3,"Empresa":"Axtente"],["Cargo":"Recepcionista","años":5,"Empresa":"Holiday"]]
+baseEmpresa['101567786']=['Nombre':'Paola','Apellido':'Ruiz','Segnom':'Andrea','Nickname':'Panru',
+'Empleos':[["Cargo":"Secretaria","Años":2,"Empresa":"IndustriaInc"],["Cargo":"Auxiliar","Años":3,"Empresa":"Axtente"],["Cargo":"Recepcionista","Años":5,"Empresa":"Holiday"]]
  ,"Direccion":"Transversal 50 Diagonal 12 -12"]
  
-baseEmpresa['1022864345']=['Nombre':'Lorena','apellido':'Rincon','segnom':'Marcela','nickname':'Lomari',
-'Empleos':[["Cargo":"Gerente talento humano","Años":2,"Empresa":"Humanoide"],["Cargo":"Supervisor","años":3,"Empresa":"RecursoH"],["Cargo":"Analista","años":5,"Empresa":"TalentX"]]
+baseEmpresa['1022864345']=['Nombre':'Lorena','Apellido':'Rincon','Segnom':'Marcela','Nickname':'Lomari',
+'Empleos':[["Cargo":"Gerente talento humano","Años":2,"Empresa":"Humanoide"],["Cargo":"Supervisor","Años":3,"Empresa":"RecursoH"],["Cargo":"Analista","Años":5,"Empresa":"TalentX"]]
  ,"Direccion":"Avenida 68 calle 77 - 15"]
  
 //Validaciones forma de obtener informacion.
@@ -49,7 +49,7 @@ baseEmpresa.each{empleados ->
  }
 //Validacion manual del contenido del primer campo de informacion del empleado.
 def formatoContratacion = ['Cedula**':['Nombre**':'','Apellido**':'','Segnom':'','Nickname**':'','Empleos':[],'Direccion**':'']]
-def nombreMapa=['70569396':['Nombre':'','Apellido':'Marin','Segnom':'','Nickname':'Paz','Empleos':[],'Direccion':'Av carrera 33 casa esquina verde']]
+def nombreMapa=['70569396':['Nombre':'Kevin','Apellido':'Marin','Segnom':'','Nickname':'Paz','Empleos':[],'Direccion':'Av carrera 33 casa esquina verde']]
 println(nombreMapa)
 
 //Validacion manual de todos los campos declarados como obligatorios e inicio de la formacion de funcion para validacion generica.
@@ -79,3 +79,23 @@ def validacionCampos(mapaAspirante){
 }
 def validacion=validacionCampos(nombreMapa)
 println (validacion)
+
+//Integracion de funciones de validacion y contratacion.
+def contratarEmpleado (base, mapaEmpleado){
+  if (validacionCampos(mapaEmpleado) == 'Aprobado'){
+    mapaEmpleado.each{aspirante  ->
+      registro = aspirante.key
+      info = aspirante.value
+      base[registro]= info
+      println('Creacion exitosa')
+    }
+  }
+  else{
+    println('Imposible crear, revise datos')
+  }
+}
+contratarEmpleado(baseEmpresa,nombreMapa)
+
+baseEmpresa.each{empleados ->
+  println(empleados)
+ }
